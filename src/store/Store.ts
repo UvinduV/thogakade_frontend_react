bb4b1084c9a3cfc8d193f655980026c7979044b4
UvinduV <uvindusanketh470@gmail.com>
2025-01-28 12:24:21 +0530

    create Store.ts

---
+++ src/store/Store.ts
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit";
+import CustomerSlice from "../reducers/CustomerSlice.ts";
+
+export const store = configureStore({
+    reducer: {
+        customers: CustomerSlice
+    }//
+})
